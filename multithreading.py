d9ea3c730bf4008f97230fa4dce6e829685773d8
Kelsey <32599136+kelseyflap@users.noreply.github.com>
2017-11-01 14:38:04 -0500

    Update multithreading.py

--- multithreading.py
+++ multithreading.py
@@ -10,7 +10,7 @@ def func1(delay, repeats):
 def func2(delay, repeats):
     while repeats > 0:
         time.sleep(delay)
-        word = input("Enter a word and I will repeat it: ") 
+        word = input("Enter a word and I will repeat it: ") # this part is overridden by the output text
         print(word)
         repeats -= 1
         
@@ -19,6 +19,7 @@ def Main():
     f2 = Thread(target = func2, args = (2, 2))
     f1.start()
     f2.start()
+    # stopping threads abruptly is apparently dangerous and there's no way of doing it safely through the multithreading module so I left that alone
     
 if __name__ == '__main__':
     Main()
