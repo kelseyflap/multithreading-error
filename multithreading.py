4a79506ecb47c2557954da24c610cf54fb3004bb
John Bartucz <john@bartucz.com>
2017-11-04 10:22:36 -0500

    adding "join" method at the bottom
    1. removed all the "Main" stuff that you don't need if you're just running this file as its own program.
    
    2. if you "join" the threads to the main program, they will not die when the main program dies. The main program will wait for each thread to finish before exiting.

--- multithreading.py
+++ multithreading.py
@@ -1,26 +1,27 @@
 from threading import Thread
 import time
 
+
 def func1(delay, repeats):
     while repeats > 0:
-        time.sleep(delay)
-        print("This is a message.")
+        print("This is message # " + str(11-repeats))
         repeats -= 1
-    
+        time.sleep(delay)
+
+
 def func2(delay, repeats):
     while repeats > 0:
-        time.sleep(delay)
-        word = input("Enter a word and I will repeat it: ") # this part is overridden by the output text
+        word = input("Enter a word and I will repeat it: ")
         print(word)
+        time.sleep(delay)
         repeats -= 1
-        
-def Main():
-    f1 = Thread(target = func1, args = (1, 5))
-    f2 = Thread(target = func2, args = (2, 2))
-    f1.start()
-    f2.start()
-    # stopping threads abruptly is apparently dangerous and there's no way of doing it safely through the multithreading module so I left that alone
-    
-if __name__ == '__main__':
-    Main()
-    
+
+f1 = Thread(target=func1, args=(1, 10))
+f2 = Thread(target=func2, args=(2, 5))
+f1.start()
+f2.start()
+
+# if you "join" the threads to the main program, they will not die when the main program dies.
+# The main program will wait for each thread to finish before exiting.
+f1.join()
+f2.join()
